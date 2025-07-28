1f84c8ac74f851f77eb084ce1eb19a2bd4e35916
Jacqueline Cuenca <jacuenca@espol.edu.ec>
2025-07-28 13:51:40 -0500

    Archivos subidos

---
+++ SistemaCitasMedicas/src/main/java/ExamenLaboratorio.java
@@ -0,0 +1,22 @@
+
+public class ExamenLaboratorio {
+
+    private Paciente paciente;
+    private String tipoExamen;
+    private String resultado;
+
+    public ExamenLaboratorio(Paciente paciente, String tipoExamen) {
+        this.paciente = paciente;
+        this.tipoExamen = tipoExamen;
+        this.resultado = "Pendiente";
+    }
+
+    public void registrarResultado(String resultado) {
+        this.resultado = resultado;
+    }
+
+    @Override
+    public String toString() {
+        return "Examen: " + tipoExamen + " - Paciente: " + paciente + " - Resultado: " + resultado;
+    }
+}
